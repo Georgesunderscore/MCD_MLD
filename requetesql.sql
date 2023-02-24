182da839a24f82bff5a3318b31ec0f9729754e34
georgesunderscore <georgeskayal2014@gmail.com>
2023-02-24 16:59:16 +0100

    commit sql 14

--- requetesql.sql
+++ requetesql.sql
@@ -28,7 +28,7 @@ ORDER BY   COUNT(*)  DESC;
 
 #5. Nom, date et lieu des batailles, classées de la plus récente à la plus ancienne (dates affichéesau format jj/mm/aaaa).
 
-SELECT b.nom_bataille, l.nom_lieu ,DATE_FORMAT(b.date_bataille,"%d/%m/%Y") from bataille b
+SELECT b.nom_bataille, l.nom_lieu ,DATE_FORMAT(b.date_bataille,"%d/%m/-%Y") from bataille b
 INNER JOIN lieu l ON b.id_lieu = l.id_lieu
 ORDER BY b.date_bataille DESC;
 
@@ -48,13 +48,13 @@ WHERE p.nom_potion ="Santé";
 
 #8. Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village gaulois'.
 
-SELECT COUNT(*) c, p.nom_personnage pnom, b.nom_bataille nomba from bataille b
+SELECT SUM(pc.qte)  c, p.nom_personnage pnom, b.nom_bataille nomba from bataille b
 INNER JOIN prendre_casque pc ON b.id_bataille = pc.id_bataille
 INNER JOIN personnage p ON pc.id_personnage = p.id_personnage
 WHERE b.nom_bataille = 'Bataille du village gaulois'
 GROUP BY p.nom_personnage , b.nom_bataille
 HAVING c IN (SELECT MAX(t.c) FROM 
-											(SELECT COUNT(*)  c, p.nom_personnage pnom, b.nom_bataille nomba from bataille b
+											(SELECT SUM(pc.qte)  c, p.nom_personnage pnom, b.nom_bataille nomba from bataille b
 											INNER JOIN prendre_casque pc ON b.id_bataille = pc.id_bataille
 											INNER JOIN personnage p ON pc.id_personnage = p.id_personnage
 											WHERE b.nom_bataille = 'Bataille du village gaulois'
@@ -92,12 +92,37 @@ ORDER BY COUNT(*) DESC;
 
 #12. Nom des potions dont un des ingrédients est le poisson frais.
 
-SELECT distinct p.nom_potion , i.nom_ingredient FROM potion p
+SELECT distinct p.nom_potion FROM potion p
 INNER JOIN composer c ON  p.id_potion= c.id_potion
 INNER JOIN ingredient i ON  c.id_ingredient= i.id_ingredient
-WHERE i.nom_ingredient = 'Poisson frais'
+WHERE i.nom_ingredient = 'Poisson frais';
 
-	
 
+#13. Nom du / des lieu(x) possédant le plus d'habitants, en dehors du village gaulois.
+
+SELECT l.nom_lieu ,COUNT(*) c from personnage p
+INNER JOIN lieu l ON p.id_lieu = l.id_lieu
+GROUP BY l.id_lieu , l.nom_lieu 
+HAVING c IN (SELECT MAX(t.c) FROM 
+				(SELECT l.nom_lieu ,COUNT(*) c from personnage p
+				 INNER JOIN lieu l ON p.id_lieu = l.id_lieu
+				 GROUP BY l.id_lieu , l.nom_lieu 
+				 HAVING l.nom_lieu <> 'Village gaulois') t
+				);
+				
+				
+#14. Nom des personnages qui n'ont jamais bu aucune potion.
+
+SELECT p.nom_personnage from personnage p
+LEFT JOIN boire b ON p.id_personnage =b.id_personnage
+WHERE b.id_potion IS NULL;
+
+#15. Nom du / des personnages qui n'ont pas le droit de boire de la potion 'Magique'.
+
+SELECT * FROM personnage per
+WHERE per.nom_personnage NOT IN (SELECT p.nom_personnage from personnage p
+											INNER JOIN autoriser_boire ab ON p.id_personnage =ab.id_personnage
+											INNER JOIN potion po ON ab.id_potion=po.id_potion
+											WHERE po.nom_potion = 'Magique')
 
 
